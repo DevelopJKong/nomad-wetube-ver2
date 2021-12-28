befbde5375612a1e9a89e34627f4ef4f50c53166
DevelopJKong <ybaram40@gmail.com>
2021-12-29 00:12:42 +0900

    #3.4 Recap 완료

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import express from "express";
 
-console.log("let's goooo");
+const PORT = 4050;
+
+const app = express();
+
+const handleHome = (req,res) => res.send("hi");
+
+const handleLogin = (req,res) => res.send("login");
+
+
+app.get("/",handleHome);
+app.get("/login",handleLogin);
+
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 😎`);
+
+app.listen(PORT, handleListening);
