8fab33a1ce4bc5e5739662704a0401567eca8d50
DevelopJKong <ybaram40@gmail.com>
2022-01-06 13:21:52 +0900

    #6.23 Middlewares 완료

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,39 +1,77 @@
-import Video from "../model/Video"
+import Video from "../model/Video";
 // Video.find({},(error,videos) =>{
 //   console.log("errors",error);
 //   console.log("videos",videos);
 // });
 
-export const home = async(req, res) => {
-    const videos = await Video.find({});
-    return res.render("home",{pageTitle:"Home",videos});
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching `});
+  const video = await Video.findById(id); //exec 가 되었을때는 정확히 어떤것인지 제대로 알아두어야 할거 같다
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing  `});
+  const video = await Video.findById(id); // 여기서는 이렇게 해주는것이 좋다
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 export const postEdit = (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.exists({ _id: id }); // post에서는 video가 있는지 없는지만 확인하면 되기 때문에 exist가 더 정확하다
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
 
-  videos[id - 1].title = title;
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
+
   return res.redirect(`/videos/${id}`);
 };
 
-export const getUpload = (req,res) => {
-  return res.render("upload",{pageTitle:"Upload Video"});
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req,res) => {
+export const postUpload = async (req, res) => {
   // here we will add a video to the videos array
-  const {title} = req.body;
-  
-  return res.redirect("/");
-}
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: Date.now(),
+      hashtags: hashtags
+        .split(",")
+        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      meta: {
+        views: 0,
+        rating: 0,
+      },
+    });
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+};
--- src/model/Video.js
+++ src/model/Video.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title:String,
-    description:String,
-    createdAt: Date,
-    hashtags: [{type:String}],
-    meta: {
-        views: Number,
-        rating: Number,
-    }
+  title: { type: String, required: true , trim: true,minLength:5},
+  description: { type: String, required: true, trim:true,maxLength:50 },
+  createdAt: { type: Date, required: true, default: Date.now },
+  hashtags: [{ type: String,trim: true }],
+  meta: {
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
+  },
 });
 
-const Video= mongoose.model("Video",videoSchema);
+const Video = mongoose.model("Video", videoSchema);
 
 export default Video;
