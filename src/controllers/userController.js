00a37a0c422b02545177eba4bf01ee17a8bd04bf
DevelopJKong <ybaram40@gmail.com>
2022-01-09 14:10:52 +0900

    #7.2 Creating Account part Three 완료

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,22 +1,32 @@
-export const getJoin = (req,res) => {
-    return res.render("join",{pageTitle:"Join"});
-}
+import User from "../model/User";
 
-export const postJoin = (req,res) => {
-    return res.redirect("/");
-}
+export const getJoin = (req, res) => {
+  return res.render("join", { pageTitle: "Join" });
+};
 
-export const edit = (req,res) => {
-    return res.send("edit");
-}
-export const remove = (req,res) => {
-    return res.send("remove");
-}
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, location } = req.body;
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  return res.redirect("/login");
+};
 
-export const login = (req,res) => {
-    return res.send("Login");
-}
+export const edit = (req, res) => {
+  return res.send("edit");
+};
+export const remove = (req, res) => {
+  return res.send("remove");
+};
 
-export const logout = (req,res) => {
-    return res.send("Logout");
-}
+export const login = (req, res) => {
+  return res.send("Login");
+};
+
+export const logout = (req, res) => {
+  return res.send("Logout");
+};
