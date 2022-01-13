05fb80c6f8fe85ed924513401bb995d4e36a5008
DevelopJKong <ybaram40@gmail.com>
2022-01-13 12:27:40 +0900

    #7.15 Environment Variables 완료

--- src/init.js
+++ src/init.js
@@ -1,7 +1,9 @@
+import "dotenv/config";  // 왜 이렇게 하면 잘 작동되는지 그래도 제대로 알아두어야 할거 같다
 import "./db";
 import "./model/Video";
 import "./model/User";
 import app from "./server";
+
 
 const PORT = 4080;
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routes/rootRouter";
 import userRouter from "./routes/userRouter";
 import videoRouter from "./routes/videoRouter";
@@ -15,9 +16,13 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));// 이렇게 사용하면 자바스크립트를 이해할수있도록 해준다
 
 app.use(session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false, //초기화 하지 않은 사용자는 쿠키를 저장하지 않는다는것 
+    // cookie:{        //쿠키를 얼마나 유지할건지 지정해준다
+    //     maxAge:20000
+    // },
+    store:MongoStore.create({ mongoUrl: process.env.DB_URL})
 }));
 
 
