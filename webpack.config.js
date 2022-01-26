070bb712b99f2c4918ac4ec5e1a91018bcd50228
DevelopJKong <ybaram40@gmail.com>
2022-01-26 19:43:53 +0900

    #9.3 Webpack Configuration part Three 완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
+const path = require("path");
+
 module.exports = {
 	entry: "./src/client/js/main.js",
+	mode:"development",
 	output: {
 		filename:"main.js",
-		path: "./assets/js",
+		path: path.resolve(__dirname,"assets","js"),
+	},
+	
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader', //javascript 코드를 babel-loader라는 loader로 가공
+					options: {
+						presets: [["@babel/preset-env",{targets: "defaults"}]],
+					}
+				}
+			}
+		]
 	}
 };
