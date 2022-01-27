fc14af65c571d2f33f978c505c4f2273fad861b7
DevelopJKong <ybaram40@gmail.com>
2022-01-27 15:32:57 +0900

    #9.5 MiniCssExtractPlugin 완료

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-	entry: "./src/client/js/main.js",
-	mode:"development",
-	output: {
-		filename:"main.js",
-		path: path.resolve(__dirname,"assets","js"),
-	},
-	
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader', //javascript 코드를 babel-loader라는 loader로 가공
-					options: {
-						presets: [["@babel/preset-env",{targets: "defaults"}]],
-					}
-				}
-			}
-		]
-	}
+  entry: "./src/client/js/main.js",
+  mode: "development",
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  output: {
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader", //javascript 코드를 babel-loader라는 loader로 가공
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+		use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], // 역순으로 적어주는 이유는 webpack이 역순으로 실행하기 때문이다
+      },
+    ],
+  },
 };
