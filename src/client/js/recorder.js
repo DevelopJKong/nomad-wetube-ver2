17186b37e6f91ce69d75eab1b02235fd738c5058
DevelopJKong <ybaram40@gmail.com>
2022-02-07 15:32:05 +0900

    #13.5 Recap 완료

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,54 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream; // 기존에는 init 함수 안에 stream을 const로 선언을 했는데 이렇게 하면
+// 서로 공유를 하지 못하기 때문에 이렇게 밖으로 빼서 let으로 선언하였습니다
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm"; //download에 대해서 제대로 알아두어야 할거 같습니다
+  document.body.appendChild(a);
+  a.click();
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Record";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
+  console.log(recorder);
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
-    video.srcObject = stream;
-    video.play();  
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
+
+init();
